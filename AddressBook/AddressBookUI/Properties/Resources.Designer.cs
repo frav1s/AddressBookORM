8a5dd7c88566668c56901e697d3b9358bc79a1d5
worst1nme <bbk-2001@mail.ru>
2021-10-21 09:22:36 +0300

    Added partical class and refractored code

--- AddressBook/AddressBookUI/Properties/Resources.Designer.cs
+++ AddressBook/AddressBookUI/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace AddressBook.Properties {
+namespace AddressBookUI.Properties {
     using System;
     
     

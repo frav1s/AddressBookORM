fa6b88b0736253d14edd218634d2e3eb0c928dde
worst1nme <bbk-2001@mail.ru>
2021-10-25 22:55:45 +0300

    update properties

--- AddressBook/AddressBookUI/Properties/Resources.Designer.cs
+++ AddressBook/AddressBookUI/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace AddressBookUI.Properties {
+namespace AddressBook.Properties {
     using System;
     
     
